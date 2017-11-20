66bb4c0d71db0b4d6d23262610c872d23e825b5b
kckrishna25 <krishna.k.kc@gmail.com>
2017-11-20 12:31:07 -0600

    initial commit

---
+++ src/main/java/model/Task.java
@@ -0,0 +1,53 @@
+package model;
+
+public class Task {
+
+    private int id;
+    private String task;
+    private String dueDate;
+    private String priority;
+    private String category;
+
+    public Task(int id, String task, String dueDate, String priority, String category) {
+        this.id = id;
+        this.task = task;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) { this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getPriority() { return priority; }
+
+    public void setPriority(String priority) {this.priority = priority; }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
