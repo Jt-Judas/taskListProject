3dbf110f6a8b30d9fb1c8346269195807682d170
Jt-Judas <judastadeo@ymail.com>
2017-11-21 17:19:45 -0600

    Added head attribute

--- src/main/java/model/Team.java
+++ src/main/java/model/Team.java
@@ -5,7 +5,7 @@ public class Team {
     private  int id;
     private  String name;
     private String description;
-private  User head;
+    private  User head;
     public Team(int id, String name, String description, User head) {
         this.id = id;
         this.name = name;
