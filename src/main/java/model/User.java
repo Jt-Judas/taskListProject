e2a2dcf517b2671269000188841ab8215caa39fc
Jt-Judas <judastadeo@ymail.com>
2017-11-21 12:30:25 -0600

    Added Team and User classes in the Model

---
+++ src/main/java/model/User.java
@@ -0,0 +1,80 @@
+package model;
+
+public class User {
+
+    private int id;
+    private String fName;
+    private String lName;
+    private String phone;
+    private String email;
+    private Team team;
+
+    public User(int id, String fName, String lName, String phone, String email, Team team) {
+        this.id = id;
+        this.fName = fName;
+        this.lName = lName;
+        this.phone = phone;
+        this.email = email;
+        this.team = team;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", team=" + team +
+                '}';
+    }
+}
